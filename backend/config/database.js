99933664a344b57e59e8f4e7ea8353a0d65ee309
Paivs <gustavo.paiva.gp1@gmail.com>
2025-05-29 17:38:28 -0300

    FEAT teste de conexão bd

--- backend/config/database.js
+++ backend/config/database.js
@@ -17,4 +17,16 @@ const sequelize = new Sequelize(
   }
 );
 
+// Teste de conexão
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+  } catch (error) {
+    console.error('Não foi possível conectar ao banco de dados:', error);
+  }
+}
+
+testConnection();
+
 module.exports = sequelize;
