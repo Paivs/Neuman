4a64e4118c65e28ed968e2dc449925fde355f579
Paivs <gustavo.paiva.gp1@gmail.com>
2025-05-28 21:03:49 -0300

    FIX conexao banco de dados

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,21 @@
 const { Sequelize } = require('sequelize');
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, './db.env') });
+require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: 'localhost',
-  dialect: 'postgres',
-  port: 5432,
-  logging: false
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: process.env.NODE_ENV === 'development' ? console.log : false,
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+  }
+);
 
 module.exports = sequelize;
